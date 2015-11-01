648dcad359c00fb26fd8a0294eee2d70f18f2a2f
pb <david4750@163.com>
2015-11-01 23:52:22 +0800

    解决冲突并合并YJJ代码

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -2,7 +2,7 @@
 
 #include "cocos2d.h"
 #include "HelloWorldScene.h"
-#include "Component/Cannon.h"
+#include "LoginScene.h"
 
 #include "CCEGLView.h"
 
@@ -107,12 +107,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
 	// set FPS. the default value is 1.0/60 if you don't call this
 	pDirector->setAnimationInterval(1.0 / 60);
 
-	// handle keyboard
-	pDirector->getOpenGLView()->setAccelerometerKeyHook(Cannon::keyboardHook);
-
 	// create a scene. it's an autorelease object
-	CCScene *pScene = HelloWorld::scene();
-
+	//CCScene *pScene = HelloWorld::scene();
+	CCScene *pScene = LoginScene::scene();
 	// run
 	pDirector->runWithScene(pScene);
 
